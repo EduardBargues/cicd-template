a03ec106721186760e032028ed1e70f7d1417559
Eduard Bargués <ebargues@ohpen.com>
2021-07-19 15:23:40 +0200

    ci:  improveperformance tests

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -15,8 +15,7 @@ logAction()
 ## SCRIPT ##
 ############
 set -e
-logAction "RUNNING E2E TESTS AGAINST"
-cat ./app.json
+logAction "RUNNING E2E TESTS"
 cp ./app.json ./tests/app.json
 cd ./tests/e2e
 npm i
