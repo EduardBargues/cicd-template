a91fa652a7b3e6cfb69c66f48a07f765b75ab5fc
Eduard Bargués <ebargues@ohpen.com>
2021-07-23 14:24:28 +0200

    fix:  include BUCKET_NAME when deleting artifacts

--- scripts/delete-artifacts.sh
+++ scripts/delete-artifacts.sh
@@ -29,14 +29,14 @@ logKeyValuePair "version" $version
 logAction "DELETING IaC FROM S3 BUCKET"
 iacS3Key="$SERVICE_NAME/$version/$SERVICE_NAME-$version-terraform.zip"
 logKeyValuePair "iac-s3-key" $iacS3Key
-aws s3api delete-object --bucket $ARTIFACTS_BUCKET_NAME --key $iacS3Key || true
+aws s3api delete-object --bucket $BUCKET_NAME --key $iacS3Key || true
 
 logAction "DELETING dotnet Src FROM S3 BUCKET"
 srcS3Key="$SERVICE_NAME/$version/$SERVICE_NAME-$version-dotnet.zip"
 logKeyValuePair "src-s3-key" $srcS3Key
-aws s3api delete-object --bucket $ARTIFACTS_BUCKET_NAME --key $srcS3Key || true
+aws s3api delete-object --bucket $BUCKET_NAME --key $srcS3Key || true
 
 logAction "DELETING nodejs Src FROM S3 BUCKET"
 srcS3Key="$SERVICE_NAME/$version/$SERVICE_NAME-$version-nodejs.zip"
 logKeyValuePair "src-s3-key" $srcS3Key
-aws s3api delete-object --bucket $ARTIFACTS_BUCKET_NAME --key $srcS3Key || true
+aws s3api delete-object --bucket $BUCKET_NAME --key $srcS3Key || true
