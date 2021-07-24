2f1eff9b5c7ac52e9423201510b4b53586d252da
Eduard Bargués <ebargues@ohpen.com>
2021-07-24 16:44:19 +0200

    feat: logs for all lambdas main

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,14 +1,15 @@
 locals {
-  prefix               = "${var.environment}-${var.service_name}-${var.service_group}"
-  prefix_dotnet        = "${local.prefix}-dotnet"
-  prefix_nodejs        = "${local.prefix}-nodejs"
-  prefix_python        = "${local.prefix}-python"
-  lambda_s3_key_dotnet = "artifacts/${var.service_name}/${var.service_version}/${var.service_name}-${var.service_version}-dotnet.zip"
-  lambda_s3_key_nodejs = "artifacts/${var.service_name}/${var.service_version}/${var.service_name}-${var.service_version}-nodejs.zip"
-  lambda_s3_key_python = "artifacts/${var.service_name}/${var.service_version}/${var.service_name}-${var.service_version}-python.zip"
-  dotnet_endpoint      = "dotnet"
-  nodejs_endpoint      = "nodejs"
-  python_endpoint      = "python"
+  prefix                 = "${var.environment}-${var.service_name}-${var.service_group}"
+  prefix_dotnet          = "${local.prefix}-dotnet"
+  prefix_nodejs          = "${local.prefix}-nodejs"
+  prefix_python          = "${local.prefix}-python"
+  lambda_s3_key_dotnet   = "artifacts/${var.service_name}/${var.service_version}/${var.service_name}-${var.service_version}-dotnet.zip"
+  lambda_s3_key_nodejs   = "artifacts/${var.service_name}/${var.service_version}/${var.service_name}-${var.service_version}-nodejs.zip"
+  lambda_s3_key_python   = "artifacts/${var.service_name}/${var.service_version}/${var.service_name}-${var.service_version}-python.zip"
+  dotnet_endpoint        = "dotnet"
+  nodejs_endpoint        = "nodejs"
+  python_endpoint        = "python"
+  logs_retention_in_days = 1
   tags = {
     "service-name"    = var.service_name
     "service-version" = var.service_version
