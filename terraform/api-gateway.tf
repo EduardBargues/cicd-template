81ec63cdd16148f79c123da3e76a0fd55ad43c32
Eduard <eduardbargues@gmail.com>
2021-07-25 20:11:40 +0200

    feat:  include apigw logs (#49)

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -44,6 +44,17 @@ resource "aws_api_gateway_stage" "api" {
   deployment_id = aws_api_gateway_deployment.api.id
 }
 
+resource "aws_api_gateway_method_settings" "apigw" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.api.stage_name
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     sid     = "AssumeRole"
