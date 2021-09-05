a1daa1ced3a3b8b6c4a0c4bc1ac9dfb8ae0a7b13
Eduard Bargués <ebargues@ohpen.com>
2021-09-05 02:43:09 +0200

    fix:  remove untagged images from ecr

--- scripts/aws/ecr/remove-image-tag-from-ecr.sh
+++ scripts/aws/ecr/remove-image-tag-from-ecr.sh
@@ -7,7 +7,12 @@ REPOSITORY_NAME=$1
 log_key_value_pair "repository-name" $REPOSITORY_NAME
 TAG=$(echo "$2"|tr '/' '-')
 log_key_value_pair "tag" $TAG
+AWS_REGION=$3
+log_key_value_pair "aws-region" $AWS_REGION
 
 aws ecr batch-delete-image \
     --repository-name $REPOSITORY_NAME \
     --image-ids imageTag=$TAG
+    
+IMAGES_TO_DELETE=$( aws ecr list-images --region $AWS_REGION --repository-name $REPOSITORY_NAME --filter "tagStatus=UNTAGGED" --query 'imageIds[*]' --output json )
+aws ecr batch-delete-image --region $ECR_REGION --repository-name $ECR_REPO --image-ids "$IMAGES_TO_DELETE" || true
