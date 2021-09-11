62394ab41d7ef95f1f1ba53d8f0ac136927c6c4c
Eduard <eduardbargues@gmail.com>
2021-09-11 14:35:17 +0200

    refactor: clean up local load testing folder (#71)

--- local-testing/scripts/run-load-tests.sh
+++ local-testing/scripts/run-load-tests.sh
@@ -1,9 +1,12 @@
 # check out dashboard here => http://localhost:3000/d/k6/k6-load-testing-results
+WORKING_FOLDER=$(pwd)
 ./scripts/docker/docker-build.sh \
     dockerfile.create-dotnet-webapi-image \
     dotnet-webapi
 ./scripts/docker/docker-build.sh \
     dockerfile.create-nodejs-server-image \
     nodejs-server
+cd $WORKING_FOLDER/local-testing
 docker-compose up -d influxdb grafana dotnet-webapi nodejs-server
-docker-compose run --rm k6 run /scripts/local-performance-tests.js
+docker-compose run --rm k6 run /scripts/load-tests.js
+cd $WORKING_FOLDER
