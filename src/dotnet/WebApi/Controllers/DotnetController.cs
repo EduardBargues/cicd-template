68828b105fa3bca9657591210357d274b714b6dd
Eduard Bargués <ebargues@ohpen.com>
2021-09-12 16:51:30 +0200

    feat:  add Function inside webapi using DI

--- src/dotnet/WebApi/Controllers/DotnetController.cs
+++ src/dotnet/WebApi/Controllers/DotnetController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Function;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -14,10 +15,12 @@ namespace WebApi.Controllers
     public class DotnetController : ControllerBase
     {
         private readonly ILogger<DotnetController> _logger;
+        private readonly IFunction _function;
 
-        public DotnetController(ILogger<DotnetController> logger)
+        public DotnetController(ILogger<DotnetController> logger, IFunction function)
         {
             _logger = logger;
+            _function = function;
         }
 
         [HttpGet("dotnet-webapi")]
@@ -25,11 +28,17 @@ namespace WebApi.Controllers
         {
             var request = (APIGatewayProxyRequest)HttpContext.Items["LambdaRequestObject"];
             var context = (ILambdaContext)HttpContext.Items["LambdaContext"];
-            _logger.LogInformation($@"DOTNET-FUNCTION LAMBDA EXECUTION
+            _logger.LogInformation($@"DOTNET-WEBAPI LAMBDA EXECUTION
     request-id: {request?.RequestContext?.RequestId}
     aws-request-id: {context?.AwsRequestId}");
 
-            return Ok("dotnet-webapi");
+            var functionResponse = _function.FunctionHandler(request, context);
+            var result = new ObjectResult(functionResponse.Body)
+            {
+                StatusCode = functionResponse.StatusCode,
+            };
+
+            return result;
         }
     }
 }
